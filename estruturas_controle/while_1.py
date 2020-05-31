ba9de757cc28f7b1da7fa157fe2001521131fb09
Everton Silva <57024315+evertonos2312@users.noreply.github.com>
2020-05-30 23:36:25 -0300

    Aula 1 sobre o uso do While com loop infinito

---
+++ estruturas_controle/while_1.py
@@ -0,0 +1,8 @@
+from random import randint
+numero_informado = -1
+numero_secreto = randint(0, 9)
+
+while numero_informado != numero_secreto:
+    numero_informado = int(input('Informe um número: '))
+
+print(f'Número secreto {numero_secreto} foi encontrado!')
